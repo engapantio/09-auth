e84675caa1874d40a3281b8953a7d1b681869d5c
Bohdan Kozar <203163016+engapantio@users.noreply.github.com>
2025-11-02 13:41:34 +0100

    Initial commit

---
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -0,0 +1,56 @@
+// app/@modal/(.)notes/[id]/page.tsx
+
+import { Metadata } from 'next';
+import {
+  QueryClient,
+  HydrationBoundary,
+  dehydrate,
+} from '@tanstack/react-query';
+import { fetchNoteById } from '@/lib/api';
+import NotePreviewClient from '@/app/@modal/(.)notes/[id]/NotePreview.client';
+
+interface NoteProps {
+  params: Promise<{ id: string }>;
+}
+
+export async function generateMetadata({
+  params,
+}: NoteProps): Promise<Metadata> {
+  const { id } = await params;
+  const note = await fetchNoteById(id);
+  return {
+    title: `Note: ${note.title} `,
+    description: `${note.content.slice(0, 32)}`,
+    openGraph: {
+      title: `Note: ${note.title} `,
+      description: `${note.content.slice(0, 32)}`,
+      url: `https://notehub.com/notes/${id}`,
+      images: [
+        {
+          url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+          width: 1200,
+          height: 630,
+          alt: 'NoteHub',
+        },
+      ],
+    },
+  };
+}
+
+const NotePreview = async ({ params }: NoteProps) => {
+  const { id } = await params;
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['note', id],
+    queryFn: () => fetchNoteById(id),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NotePreviewClient />
+    </HydrationBoundary>
+  );
+};
+
+export default NotePreview;
---
+++ lib/api.ts
@@ -0,0 +1,60 @@
+// lib/api.ts
+
+import axios from 'axios';
+import { Note } from '../types/note';
+interface FetchNotesResponse {
+  notes: Note[];
+  totalPages: number;
+}
+
+export interface CreateNoteData {
+  title: string;
+  tag: 'Todo' | 'Work' | 'Personal' | 'Meeting' | 'Shopping';
+  content: string;
+}
+
+axios.defaults.baseURL = 'https://notehub-public.goit.study/api';
+axios.defaults.headers.common['Authorization'] =
+  process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
+
+export const fetchNotes = async (
+  search: string,
+  page: number,
+  tag: string,
+  perPage: number = 12
+): Promise<FetchNotesResponse> => {
+  const searchQuery = search ? search : '';
+
+  const params = tag
+    ? {
+        search: searchQuery,
+        tag,
+        page,
+        perPage,
+      }
+    : {
+        search: searchQuery,
+        page,
+        perPage,
+      };
+
+  const response = await axios.get<FetchNotesResponse>('/notes', {
+    params,
+  });
+  return response.data;
+};
+
+export const createNote = async (newNote: CreateNoteData): Promise<Note> => {
+  const response = await axios.post<Note>('/notes', newNote);
+  return response.data;
+};
+
+export const deleteNote = async (id: Note['id']): Promise<Note> => {
+  const response = await axios.delete<Note>(`/notes/${id}`);
+  return response.data;
+};
+
+export const fetchNoteById = async (id: Note['id']): Promise<Note> => {
+  const response = await axios.get<Note>(`/notes/${id}`);
+  return response.data;
+};
